1f29765d3b414a9ea8270acc02ce0c824235447f
Matt Werner <mrwerner392@gmail.com>
2019-04-06 17:02:58 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -3,12 +3,12 @@ var playlist = {eminem: 'Lose Yourself'};
 
 // adds a key-value pair to playlist object
 function updatePlaylist(list, artistName, songTitle) {
-  list.artistName = songTitle;
+  list[artistName] = songTitle;
   return list;
 }
 
 // removes a key-value pair from playlist object
 function removeFromPlaylist(list, artistName) {
-  delete list.artistName;
+  delete list[artistName];
   return list;
 }
